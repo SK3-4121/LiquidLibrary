94d861e65965773c0f9a9ab5cb3ff0160b94c91a
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2021-11-17 20:07:05 -0800

    Update LiquidLibraryInstaller.sh

--- RELASES/iOS/LiquidLibraryInstaller.sh
+++ RELASES/iOS/LiquidLibraryInstaller.sh
@@ -1,1 +1,1 @@
-curl -L https://raw.githubusercontent.com/SK3-4121/LiquidLibrary/main/iOS-INSTALLER/install.sh | sh
+curl -L https://raw.githubusercontent.com/SK3-4121/LiquidLibrary/main/RELASES/iOS/Install.sh | sh
